2731fc1cfcc09eae995027d8d553a40d3bb59cac
svillahermosa <s.villahermosa91@gmail.com>
2022-05-13 19:12:35 +0200

    code revision

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
         print(f'INIT - Obteniendo datos de: {account}')
         df = get_data_from_account(account)
         df_final = pd.concat([df_final, df])
-        df_final.to_csv(r"C:\Users\Alfonso\Documents\IA\ProyectoLOL\yoya.csv", sep='\t')
+        df_final.to_csv(r"C:\Users\Alfonso\Documents\IA\ProyectoLOL\yoya.csv", sep='\t')  # TODO: Cambiar por ruta genérica
         print(f'END - Obteniendo datos de: {account}')
     print(f"Total de datos conseguidos de {len(accounts)} cuentas: \n - {df_final.shape[0]} participantes \n - {int(df_final.shape[0]/10)} partidas \n - {df_final.shape[1]} columnas ")
