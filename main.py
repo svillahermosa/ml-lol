f8da366d1f064a2314bae99fce7131e6e12eca3e
unknown <carlosjavierolmedo@gmail.com>
2022-05-22 20:32:04 +0200

    Added smurf tag to matches

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    accounts = ["gg fructis", "ChicoRebelde","TheBoySavior 69","no tiene sendito","LambOrnnGinyin","KOI ÄITANA","Pyke Wazowskii", "KeyboardWRLD999", "Adryos Lepido","2023 Tercera","Amadeu Carvalho", "elmi uwu69", "PEŁUK1NG", "egirls above all", "ELMILLOR IS BACK", "IamDiamond", "Remember me mom", "abby me arruinó", "EL GRÁFICAS", "Laccek", "Aesenardo", "IreliaCosplayer", "Berri Vuelve Xfa", "KOI TheGrefg", "Barriga Humana", "heyy olviyonna", "Goku que", "Cabeza de Huevo", "MOTONAMl", "Ladguillos69", "Monje Shacolínn", "DuaLLipa", "CaiQi fangirl", "Weakside Jayce", "Beep bop SIUUUUU", "DevuelvanEl0ro", "TetelOl EnjoyeR"]
+    accounts_high_elo = ["gg fructis", "ChicoRebelde","TheBoySavior 69","no tiene sendito","LambOrnnGinyin","KOI ÄITANA","Pyke Wazowskii", "KeyboardWRLD999", "Adryos Lepido","2023 Tercera","Amadeu Carvalho", "elmi uwu69", "PEŁUK1NG", "egirls above all", "ELMILLOR IS BACK", "IamDiamond", "Remember me mom", "abby me arruinó", "EL GRÁFICAS", "Laccek", "Aesenardo", "IreliaCosplayer", "Berri Vuelve Xfa", "KOI TheGrefg", "Barriga Humana", "heyy olviyonna", "Goku que", "Cabeza de Huevo", "MOTONAMl", "Ladguillos69", "Monje Shacolínn", "DuaLLipa", "CaiQi fangirl", "Weakside Jayce", "Beep bop SIUUUUU", "DevuelvanEl0ro", "TetelOl EnjoyeR"]
+    # accounts_low_elo = ["HandSoIo", "IRELIAING69", "SoloQlona", "l Iove FUMAR", "ahri sIave", "abby una cosa", "XAM PING", "Amouranth Sister", "FRA KCasadο", "KatthyVipi", "Tetones", "TOAD AZULGRANA", "FTR ATTACK", " LittleRagerMilf", "EviIpain", "ahrivedercı", "Chica qué dices", "XaviSaltayAbraza", "YoGaneAMoscow5", "FUNADO PERO RICO"]
+    accounts_low_elo = ["l Iove FUMAR", "ahri sIave", "abby una cosa", "XAM PING", "Amouranth Sister", "FRA KCasadο", "KatthyVipi", "Tetones", "TOAD AZULGRANA", "FTR ATTACK", " LittleRagerMilf", "EviIpain", "ahrivedercı", "Chica qué dices", "XaviSaltayAbraza", "YoGaneAMoscow5", "FUNADO PERO RICO"]
     df_final = pd.DataFrame()
-    for account in accounts:
+    for account in accounts_low_elo:
         print(f'INIT - Obteniendo datos de: {account}')
         df = get_data_from_account(account)
         df_with_pk = assign_pk_to_df(df)
@@ -16,9 +18,9 @@ if __name__ == '__main__':
         df_merged = df_merged.loc[df_merged.summonerName == account]
         df_merged = df_merged.loc[df_merged.pk_partida.notnull()]
         df_final = pd.concat([df_final, df_merged])
-        df_final.to_csv(r"C:\DevelopmentLaLiga\ml-lol\extracts\full.tsv", sep='\t')  # TODO: Cambiar por ruta genérica
+        df_final.to_csv(r"C:\Users\Carlos\OneDrive\Escritorio\ML\ml-lol\extracts\full.tsv", sep='\t')  # TODO: Cambiar por ruta genérica
         print(f'END - Obteniendo datos de: {account}')
-    print(f"Total de datos conseguidos de {len(accounts)} cuentas: \n - {df_final.shape[0]} participantes \n - {int(df_final.shape[0]/10)} partidas \n - {df_final.shape[1]} columnas ")
+    print(f"Total de datos conseguidos de {len(accounts_low_elo)} cuentas: \n - {df_final.shape[0]} participantes \n - {int(df_final.shape[0]/10)} partidas \n - {df_final.shape[1]} columnas ")
 
 
 def assign_pk_to_df(df):
