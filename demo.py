221f202b388299962ff98ff672f364277b859a61
Alfonso <alfonso.roblesro@gmail.com>
2022-06-10 00:27:07 +0200

    change result demo to DF

--- demo.py
+++ demo.py
@@ -2,25 +2,27 @@ import pandas as pd
 
 from check_smurf import get_model_trained
 
-lista_jugadores = ['elmi uwu69', 'HandSoIo', 'PEŁUK1NG', 'SK Eckas', 'vladi1v9', '1keduii1', 'FakeRookie1', 'Jonah Falcon', 'El Hookeru']
 
-dic_df_jugadores = {}
+def demo():
+    lista_jugadores = ['elmi uwu69', 'HandSoIo', 'PEŁUK1NG', 'SK Eckas', 'vladi1v9', '1keduii1', 'FakeRookie1', 'Jonah Falcon', 'El Hookeru']
 
-df_full = pd.read_csv(fr"extracts/full_prepared2.tsv", sep='\t')
-columns = list(df_full.columns)
+    dic_df_jugadores = {'summonerName':[], 'percentage': []}
 
-model = get_model_trained()
+    df_full = pd.read_csv(fr"extracts/full_prepared2.tsv", sep='\t')
+    columns = list(df_full.columns)
 
-for jugador in lista_jugadores:
-    df = pd.read_csv(fr"extracts/{jugador}.tsv", sep='\t')
-    predictions = model.predict(df)
-    total_score = 0
-    for pred in predictions:
-        total_score += pred
-    print(
-        f'Probabilidad de ser smurf para el usuario {jugador}: {100 * total_score / len(predictions)} (basado en {len(predictions)} partidas)')
-    dic_df_jugadores[jugador] = 100 * total_score / len(predictions)
+    model = get_model_trained()
 
-print(dic_df_jugadores)
+    for jugador in lista_jugadores:
+        df = pd.read_csv(fr"extracts/{jugador}.tsv", sep='\t')
+        predictions = model.predict(df)
+        total_score = 0
+        for pred in predictions:
+            total_score += pred
+        print(
+            f'Probabilidad de ser smurf para el usuario {jugador}: {100 * total_score / len(predictions)} (basado en {len(predictions)} partidas)')
+        dic_df_jugadores['summonerName'].append(jugador)
+        dic_df_jugadores['percentage'].append(100 * total_score / len(predictions))
 
+    return pd.DataFrame(dic_df_jugadores)
 
